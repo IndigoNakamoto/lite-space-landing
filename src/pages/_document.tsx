a985926fb780555ea63e41145d18b5582ec3bed1
indigo <indigonakamoto@gmail.com>
2023-03-17 17:50:50 -0700

    Remove crossOrigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
 					rel="stylesheet"
