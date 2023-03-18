3915674cff061a86c518fe3acf7ade40f63cb9d0
indigo <indigonakamoto@gmail.com>
2023-03-17 17:47:08 -0700

    Restyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
+import "<prefix>/styles/globals.css";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 
-config.autoAddCss = false;
-
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
-
-export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,111 +1,44 @@
 import Head from "next/head";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faTwitter,
-	faReddit,
-	faDiscord,
-} from "@fortawesome/free-brands-svg-icons";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home: React.FC = () => {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Lite.space - Litecoin Mempool Explorer</title>
+				<title>Lite.space - Coming Soon</title>
 				<meta
 					name="description"
-					content="Lite.space, a powerful Litecoin Mempool Explorer, inspired by Mempool.space. Monitor and analyze the Litecoin network with ease."
+					content="Lite.space - Mempool Block Explorer for Litecoin"
 				/>
-
-				{/* Facebook Open Graph */}
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://lite.space" />
-				<meta
-					property="og:title"
-					content="Lite.space - Litecoin Mempool Explorer"
-				/>
-				<meta
-					property="og:description"
-					content="Lite.space, a powerful Litecoin Mempool Explorer, inspired by Mempool.space. Monitor and analyze the Litecoin network with ease."
-				/>
-				<meta property="og:image" content="https://lite.space/your-image.png" />
-
-				{/* Twitter Card */}
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:url" content="https://lite.space" />
-				<meta
-					name="twitter:title"
-					content="Lite.space - Litecoin Mempool Explorer"
-				/>
-				<meta
-					name="twitter:description"
-					content="Lite.space, a powerful Litecoin Mempool Explorer, inspired by Mempool.space. Monitor and analyze the Litecoin network with ease."
-				/>
-				<meta
-					name="twitter:image"
-					content="https://lite.space/your-image.png"
-				/>
-
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.png" />
 			</Head>
 
-			<div className={styles.content}>
-				<main className={styles.main}>
-					<h1 className={styles.title}>Lite.space</h1>
-					<p className={styles.description}>
-						A Litecoin Mempool Explorer, a fork from Mempool.space
-					</p>
-					<p className={styles.description}>Coming Soon</p>
-				</main>
-
-				<footer className={styles.footer}>
-					<div className={styles.socialIcons}>
-						<a
-							href="https://twitter.com/litespace_"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<FontAwesomeIcon icon={faTwitter} size="2x" />
-						</a>
-						<a
-							href="https://discord.gg/EksJTdeRTS"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<FontAwesomeIcon icon={faDiscord} size="2x" />
-						</a>
-					</div>
-				</footer>
-			</div>
-
-			<style jsx>{`
-				main {
-					background-image: url("../../public/Sphere (1).webm");
-					background-size: cover;
-					background-position: center;
-					display: flex;
-					flex-direction: column;
-					align-items: center;
-					justify-content: center;
-				}
-
-				.socialIcons {
-					display: flex;
-					justify-content: space-around;
-					width: 50%;
-					margin-top: 1rem;
-				}
-
-				.socialIcons a {
-					color: inherit;
-					margin: 0 1rem;
-					transition: color 0.3s ease;
-				}
-
-				.socialIcons a:hover {
-					color: #your-color;
-				}
-			`}</style>
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					<span style={{ color: "#345D9D" }}>Lite</span>
+					<span style={{ color: "#333" }}>.space</span>
+				</h1>
+				<p className={styles.p}>A Litecoin Mempool Explorer - Coming Soon</p>
+				<p className={styles.p2}>A fork of Mempool.space</p>
+				<div className={styles.socialIcons}>
+					<a
+						href="https://twitter.com/litespace_"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src="/twitter.svg" alt="Twitter Icon" width="24" height="24" />
+					</a>
+					<a
+						href="https://discord.gg/EksJTdeRTS"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src="/discord.svg" alt="Discord Icon" width="24" height="24" />
+					</a>
+				</div>
+			</main>
 		</div>
 	);
-}
+};
+
+export default Home;
